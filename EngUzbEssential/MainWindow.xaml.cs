74b0804ef3811970c8fe5149c8c309228cb7ec92
Sobitov Sardor <just.do.it.001800@gmail.com>
2025-04-03 18:20:45 +0500

    Add project files.

---
+++ EngUzbEssential/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿// MainWindow.xaml.cs
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace EngUzbEssential
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Create drop shadow effect and add to resources
+            DropShadowEffect dropShadow = new DropShadowEffect
+            {
+                Color = Colors.Black,
+                Direction = 270,
+                ShadowDepth = 10,
+                Opacity = 0.2,
+                BlurRadius = 20
+            };
+
+            this.Resources.Add("DropShadow", dropShadow);
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
